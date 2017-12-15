f27640eb98ec968067db35055d557047d3eb8be0
Nato Boram <nato.boram@gmail.com>
2017-12-15 17:57:42 -0500

    embed -> attachment

--- bot/call.go
+++ bot/call.go
@@ -202,13 +202,14 @@ func foward(s *discordgo.Session, m *discordgo.MessageCreate) {
 						if err != nil {
 							fmt.Println("Couldn't foward the message from " + fromChannel.Name + " to " + toChannel.Name + ".")
 							fmt.Println(err.Error())
+							return
 						}
 
-						// Foward embeds
-						for x := 0; x < len(m.Embeds); x++ {
-							_, err = s.ChannelMessageSendEmbed(toChannel.ID, m.Embeds[x])
+						// Foward attachments
+						for x := 0; x < len(m.Attachments); x++ {
+							_, err = s.ChannelMessageSend(toChannel.ID, m.Attachments[x].URL)
 							if err != nil {
-								fmt.Println("Couldn't foward an embed from " + fromChannel.Name + " to " + toChannel.Name + ".")
+								fmt.Println("Couldn't foward an attachment from " + fromChannel.Name + " to " + toChannel.Name + ".")
 								fmt.Println(err.Error())
 							}
 						}
